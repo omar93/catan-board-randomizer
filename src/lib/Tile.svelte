1a839c4b15fd8a47325c73613a6844b7e99bcbc4
Omar Alhamad <omar93@hotmail.se>
2024-11-16 11:38:29 +0100

    Active runes mode in Svelte Add Tile component Draw hexagon tile with SVG Update compiler option to use runes

---
+++ src/lib/Tile.svelte
@@ -0,0 +1,14 @@
+<script>
+  let tile = $state('wood')
+</script>
+
+<svg viewBox="0 0 100 100">
+  <polygon points="25 0, 0 50, 25 100, 75 100, 100 50, 75 0" stroke="black" fill="white"/>
+</svg>
+
+<style>
+  svg {
+    width: 200px;
+    height: 200px;
+  }
+</style>
