46317b2267bf3db8fb6556c8ce4706ea0207858b
Omar Alhamad <omar93@hotmail.se>
2024-11-16 15:44:00 +0100

    Add grid of hexagon Each row generates x ammount of hexagons Generate Catan game board

--- src/lib/Tile.svelte
+++ src/lib/Tile.svelte
@@ -1,14 +1,63 @@
 <script>
-  let tile = $state('wood')
-</script>
+  let { pointTop, id, row } = $props()
 
-<svg viewBox="0 0 100 100">
-  <polygon points="25 0, 0 50, 25 100, 75 100, 100 50, 75 0" stroke="black" fill="white"/>
+  const isEven = () => {
+    return id % 2 == 0
+  }
+
+  let adjuster = $derived(row === 3 ? 50 : row === 5 ? -50 : 0);
+  let left = $derived(50*id*2+adjuster)
+  let top = $derived(75)
+
+
+  
+  
+  //<polygon points="25 0, 0 50, 25 100, 75 100, 100 50, 75 0" stroke="black" fill="white"/>
+  // X , Y
+
+</script>
+<svg viewBox="0 0 100 100" id="hexagon" style="position: absolute; margin-left:{left}; margin-top:{top};" class={id%2==0 ? "even" : "odd"}>
+
+  <!--Hexagon-->
+  <defs>
+    <clipPath id="hexagon-clip">
+      <path id="hexagon"
+        d="
+        M 25 0 
+        L 0 50
+        L 25 100
+        L 75 100
+        L 100 50
+        L 75 0"
+        fill="url(#wood.bmp)"
+      />
+    </clipPath>
+  </defs>
+
+  <!--Image-->
+
+
+
+  <!--Outline-->
+  <path
+  d="
+  M 50 0
+  L 0 25
+  L 0 75
+  L 50 100
+  L 100 75
+  L 100 25
+  Z"
+  fill="none"
+  stroke="black"
+  />
+
+
 </svg>
 
 <style>
   svg {
-    width: 200px;
-    height: 200px;
+    width: 100px;
+    height: 100px;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,53 @@
   import Tile from "$lib/Tile.svelte"
 </script>
 
-<Tile/> 
+<div id="wrapper">
+
+  <div id="row1" class="row">
+    {#each {length: 3} as _, i}
+      <Tile pointTop={(i % 2 === 0) ? true : false} id={i+1} row={3}/> 
+    {/each}
+  </div>
+
+  <div id="row2" class="row">
+    {#each {length: 4} as _, i}
+      <Tile pointTop={(i % 2 === 0) ? true : false} id={i+1} row={4}/> 
+    {/each}
+  </div>
+
+  <div id="row3" class="row">
+    {#each {length: 5} as _, i}
+      <Tile pointTop={(i % 2 === 0) ? true : false} id={i+1} row={5}/> 
+    {/each}
+  </div>
+
+  <div id="row4" class="row">
+    {#each {length: 4} as _, i}
+      <Tile pointTop={(i % 2 === 0) ? true : false} id={i+1} row={4}/> 
+    {/each}
+  </div>
+
+  <div id="row5" class="row">
+    {#each {length: 3} as _, i}
+      <Tile pointTop={(i % 2 === 0) ? true : false} id={i+1} row={3}/> 
+    {/each}
+  </div>
+
+</div>
+
+<style>
+  #wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-left: 22.5%;
+    margin-top: -5%;
+  }
+
+  #wrapper > div {
+    margin-top: 75px;
+  }
+
+  .row {
+    position: relative;
+  }
+</style>
